a4d444890c7475f83db71d58b839620b3bacc18a
Prawinrajan <44932575+prawinrajan@users.noreply.github.com>
2024-10-11 02:02:20 +0530

    Practiced with Row, Column, Image

--- unit-1/ComposeTutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ unit-1/ComposeTutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,19 +1,37 @@
 package com.example.composetutorial
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
-data class Message(val author:String , val body:String);
+data class Message(val author: String, val body: String);
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,10 +39,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeTutorialTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Surface(modifier = Modifier.fillMaxSize()) {
                     MessageCard(
-                        name = "Prawin",
-                        modifier = Modifier.padding(innerPadding)
+                        Message("Prawin", "Learning Kotlin"),
+                        modifier = Modifier.padding(4.dp)
                     )
                 }
             }
@@ -32,18 +50,41 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun MessageCard(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MessageCard(msg: Message, modifier: Modifier = Modifier) {
+    Row(modifier = modifier.padding(8.dp), verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(id = R.drawable._0864806),
+            contentDescription = "Profile",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(
+                    CircleShape
+                )
+                .border(1.dp, Color.LightGray, CircleShape),
+            alignment = Alignment.BottomCenter
+        )
+        Spacer(modifier = Modifier.padding(2.dp))
+        Column {
+            Text(
+                text = msg.author,
+                modifier = modifier
+            )
+            Text(
+                text = msg.body,
+                modifier = modifier
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun MessageCardPreview() {
     ComposeTutorialTheme {
-        MessageCard("Android")
+        Surface {}
+        MessageCard(Message("Prawin", "Learning Kotlin"))
+        MessageCard(Message("Prawin", "Learning Kotlin"))
     }
 }
